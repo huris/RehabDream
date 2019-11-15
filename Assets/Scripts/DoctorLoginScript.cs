18973538e4ac9d8f00e3de652fdb393163954957
huris <huris102234@gmail.com>
2019-11-15 16:22:23 +0800

    完善按下Tab键和shift Tab实现InputField之间的转换，回车键进行登录

--- Assets/Scripts/DoctorLoginScript.cs
+++ Assets/Scripts/DoctorLoginScript.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System;
 using System.Linq;
+using UnityEngine.EventSystems;
 
 public class DoctorLoginScript : MonoBehaviour {
 
@@ -13,21 +14,64 @@ public class DoctorLoginScript : MonoBehaviour {
 
     public GameObject ErrorInformation;   // 账号或者密码错误显示
 
+    public EventSystem system;
+
+    private Selectable SelecInput;   // 当前焦点所处的Input
+    private Selectable NextInput;   // 目标Input
+
     // Use this for initialization
-    void Start () {
+    void OnEnable () {
         DoctorID = transform.Find("DoctorID").GetComponent<InputField>();    //  绑定账号
         DoctorPassword = transform.Find("DoctorPassword").GetComponent<InputField>();    //  绑定密码
 
         ErrorInformation = transform.Find("ErrorInput").gameObject;   // 绑定错误信息
         ErrorInformation.SetActive(false);     // 设置语句刚开始处于未激活状态
 
+        system = EventSystem.current;       // 获取当前的事件
+
         //DoctorDatabaseManager.instance.DeleteCS(241283);   // 数据库测试
     }
-	
-	// Update is called once per frame
-	void Update () {
 
-	}
+    // Update is called once per frame
+    void Update() {
+
+        //在Update内监听Tap键的按下
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            //是否聚焦Input
+            if (system.currentSelectedGameObject != null)
+            {
+                //获取当前选中的Input
+                SelecInput = system.currentSelectedGameObject.GetComponent<Selectable>();
+                //监听Shift
+                if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+                {
+                    //Shift按下则选择出去上方的Input
+                    NextInput = SelecInput.FindSelectableOnUp();
+                    //上边没有找左边的
+                    if (NextInput == null) NextInput = SelecInput.FindSelectableOnLeft();
+                }
+                else
+                {
+                    //没按shift就找下边的Input
+                    NextInput = SelecInput.FindSelectableOnDown();
+                    //或者右边的
+                    if (NextInput == null) NextInput = SelecInput.FindSelectableOnRight();
+                }
+            }
+
+            //下一个Input不空的话就聚焦
+            if (NextInput != null) NextInput.Select();
+        }
+
+        // 按回车键进行登录
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            LoginOnClick();
+        }
+
+    }
+   
 
     public void LoginOnClick()
     {
