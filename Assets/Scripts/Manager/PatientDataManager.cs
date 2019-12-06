e05a5ef9d5bfee8cc60cd3b89285c8cdaadffafc
981090121@qq.com <981090121@qq.com>
2019-12-06 15:47:37 +0800

    合并

--- Assets/Scripts/Manager/PatientDataManager.cs
+++ Assets/Scripts/Manager/PatientDataManager.cs
@@ -174,6 +174,8 @@ public class PatientDataManager : MonoBehaviour{
     {
         this.SuccessCount = 0;
         this.FinishCount = 0;
+        this.SoccerTrackTips = true;
+        this.WordTips = true;
         SetTrainingStartTime();
         SetTrainingEndTime();
     }
