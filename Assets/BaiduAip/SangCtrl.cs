3180eec25a459795f923d6ac1dea4ca8dafc64f0
huris <huris102234@gmail.com>
2020-03-19 09:33:30 +0800

    添加了语音的sdk包，准备加入语音功能

---
+++ Assets/BaiduAip/SangCtrl.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Wit.BaiduAip.Speech;
+using static Wit.BaiduAip.Speech.Tts;
+
+public class SangCtrl : MonoBehaviour {
+     string APIKey = "K0ZBOObtsCiqhfgW9tGuwQkT";
+     string SecretKey = "NjqjRx0EU6NuGLQjTfbrMyxjsQI5qu0Y ";
+    private Tts _asr;
+    private AudioSource _audioSource;
+  //  private bool _startPlaying;
+ 
+    void Awake () {
+        _asr = new Tts(APIKey, SecretKey);
+        StartCoroutine(_asr.GetAccessToken());
+        _audioSource = gameObject.AddComponent<AudioSource>();
+        _audioSource.volume = 0.4f;
+    }
+  /// <summary>
+  /// 语音合成
+  /// </summary>
+  /// <param name="message">合成信息的内容</param>
+    public  void SpeechSynthesis(string message)
+    {
+       
+        StartCoroutine(_asr.Synthesis(message, s =>
+        {
+            if (s.Success)
+            {
+               // DescriptionText.text = "合成成功，正在播放";
+                _audioSource.clip = s.clip;
+                _audioSource.Play();
+               
+            }
+            else
+            {
+                // DescriptionText.text = s.err_msg;
+                // SynthesisButton.gameObject.SetActive(true);
+                Debug.LogError("合成失败");
+            }
+        },5,5,(int)Pronouncer.Duyaya));
+    }
+
+
+    /// <summary>
+    /// 开始听用户声音
+    /// </summary>
+    // public Button StartButton;
+    /// <summary>
+    /// 停止听取
+    /// </summary>
+    //  public Button StopButton;
+    // private AudioClip _clipRecord;
+    // [Tooltip("语音合成")]  public Button SynthesisButton;
+
+    // public Text DescriptionText;//显示当前状态
+
+    //public void OnClickStartButton()
+    //{
+    //    StartButton.gameObject.SetActive(false);
+    //    StopButton.gameObject.SetActive(true);
+    //    DescriptionText.text = "请讲话...";
+
+    //    _clipRecord = Microphone.Start(null, false, 30, 16000);
+    //}
+
+    //public void OnClickStopButton()
+    //{
+    //    StartButton.gameObject.SetActive(false);
+    //    StopButton.gameObject.SetActive(false);
+    //    DescriptionText.text = "识别中...";
+    //    Microphone.End(null);
+    //    Debug.Log("end record");
+    //    var data = Asr.ConvertAudioClipToPCM16(_clipRecord);
+    //    StartCoroutine(_asr.Recognize(data, s =>
+    //    {
+    //        Debug.Log("未识别到语音");
+
+    //        StartButton.gameObject.SetActive(true);
+    //    }));
+    //}
+}
--- Assets/Scripts/Doctor/UI/TrainingDataInitScript.cs
+++ Assets/Scripts/Doctor/UI/TrainingDataInitScript.cs
@@ -1,40 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class TrainingDataInitScript : MonoBehaviour {
 
-    public GameObject NoTrainingData;
+	public GameObject NoTrainingData;
 
 	public GameObject Report;
 	public Toggle TrainingToggle;
 
+	public Text NoTrainingDataText;
+	public Button TrainingButton;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start() {
+
 	}
 
-    void OnEnable()
-    {
-        NoTrainingData = transform.Find("NoTrainingData").gameObject;
+	void OnEnable()
+	{
+		NoTrainingData = transform.Find("NoTrainingData").gameObject;
 		Report = transform.parent.parent.parent.Find("Report").gameObject;
 		TrainingToggle = transform.parent.parent.parent.Find("Report/Training").GetComponent<Toggle>();
 
 		//DoctorDataManager.instance.doctor.patient.TrainingPlays = DoctorDatabaseManager.instance.ReadPatientRecord(DoctorDataManager.instance.doctor.patient.PatientID, 0);
 
 		if (DoctorDataManager.instance.doctor.patient.TrainingPlays != null && DoctorDataManager.instance.doctor.patient.TrainingPlays.Count > 0) NoTrainingData.SetActive(false);
-        else NoTrainingData.SetActive(true);
-    }
-	
+		else
+		{ 
+			NoTrainingData.SetActive(true);
+
+			if (DoctorDataManager.instance.doctor.patient.PlanIsMaking)
+			{
+				TrainingButton.gameObject.SetActive(true);
+				NoTrainingDataText.text = "抱歉，该患者目前未进行相关训练\n\n请患者训练后再查看数据";
+			}
+			else
+			{
+				TrainingButton.gameObject.SetActive(false);
+				NoTrainingDataText.text = "抱歉，该患者目前暂未制定训练计划\n\n请先制定计划后开始训练";
+			}
+		}
+	}
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update() {
+
 	}
 
 	public void ReadReportButtonOnclick()
 	{
 		Report.SetActive(true);
 		TrainingToggle.isOn = true;
+	}
+
+	public void StartTraining()
+	{
+		PatientDataManager.instance.SetUserMessage(DoctorDataManager.instance.doctor.patient.PatientID, DoctorDataManager.instance.doctor.patient.PatientName, DoctorDataManager.instance.doctor.patient.PatientSex);
+		//PatientDataManager.instance.SetTrainingPlan(PatientDataManager.Str2DifficultyType(DoctorDataManager.instance.patient.trainingPlan.PlanDifficulty), DoctorDataManager.instance.patient.trainingPlan.GameCount, DoctorDataManager.instance.patient.trainingPlan.PlanCount);
+
+		TrainingPlay trainingPlay = new TrainingPlay();
+		trainingPlay.SetTrainingID(DoctorDatabaseManager.instance.ReadPatientRecordCount(0) + DoctorDatabaseManager.instance.ReadPatientRecordCount(1));
+
+		//DoctorDataManager.instance.doctor.patient.TrainingPlays.Add(trainingPlay);
+
+		PatientDataManager.instance.SetTrainingID(trainingPlay.TrainingID);
+		PatientDataManager.instance.SetMaxSuccessCount(DoctorDataManager.instance.doctor.patient.MaxSuccessCount);
+		PatientDataManager.instance.SetPlanDifficulty(PatientDataManager.Str2DifficultyType(DoctorDataManager.instance.doctor.patient.trainingPlan.PlanDifficulty));
+		PatientDataManager.instance.SetPlanCount(DoctorDataManager.instance.doctor.patient.trainingPlan.PlanCount);
+		PatientDataManager.instance.SetPlanDirection(PatientDataManager.Str2DirectionType(DoctorDataManager.instance.doctor.patient.trainingPlan.PlanDirection));
+		PatientDataManager.instance.SetPlanTime(DoctorDataManager.instance.doctor.patient.trainingPlan.PlanTime);
+		PatientDataManager.instance.SetIsEvaluated(0);
+
+		DoctorDataManager.instance.FunctionManager = 3; // 返回的时候进入训练状况查询界面
+		SceneManager.LoadScene("06-Game");  // 如果登录成功,则进入医生管理界面
 	}
 }
