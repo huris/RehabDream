fcc584b44640c04f26c56fb03a635c5dd6a83a68
huris <huris102234@gmail.com>
2020-05-25 11:49:02 +0800

    准备修改语音播报为本地

--- Assets/Scripts/Doctor/UI/SoccerBarTest.cs
+++ Assets/Scripts/Doctor/UI/SoccerBarTest.cs
@@ -4,6 +4,42 @@ using UnityEngine;
 
 public class SoccerBarTest : MonoBehaviour {
 
+    public AudioSource audiosource;
+
+    public static SoccerBarTest _instance;
+
+    void Awake()
+    {
+        audiosource = gameObject.AddComponent<AudioSource>();
+
+        audiosource.playOnAwake = false;  //playOnAwake设为false时，通过调用play()方法启用
+
+        _instance = this; //通过Sound._instance.方法调用
+        _instance.PlayMusicByName("Evaluation1");
+    }
+
+    //在指定位置播放音频 PlayClipAtPoint()
+    public void PlayAudioByName(string name)
+    {
+        //这里目标文件处在 Resources/Sounds/目标文件name
+        AudioClip clip = Resources.Load<AudioClip>("Sounds/" + name);
+        AudioSource.PlayClipAtPoint(clip, Camera.main.transform.position);
+    }
+
+    //如果当前有其他音频正在播放，停止当前音频，播放下一个
+    public void PlayMusicByName(string name)
+    {
+        AudioClip clip = Resources.Load<AudioClip>("Sounds/" + name);
+
+        if (audiosource.isPlaying)
+        {
+            audiosource.Stop();
+        }
+
+        audiosource.clip = clip;
+        audiosource.Play();
+    }
+
 	// Use this for initialization
 	void Start () {
 		
