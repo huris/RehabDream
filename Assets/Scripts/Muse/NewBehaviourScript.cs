a01a384440234de211a48fd47886cdca63a01bf3
ChenBaye <981090121@qq.com>
2020-10-14 22:26:38 +0800

    Update NewBehaviourScript.cs

--- Assets/Scripts/Muse/NewBehaviourScript.cs
+++ Assets/Scripts/Muse/NewBehaviourScript.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Muse;
@@ -8,8 +8,12 @@ public class NewBehaviourScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Debug.Log(1);
-		//OSCMonitor o = new OSCMonitor();
-		Debug.Log(2);
+		OSCMonitor o = new OSCMonitor(5000, (MuseMessage m)=> { });
+        Debug.Log(o.port);
+
+        o.StartUDPLstener();
+
+        Debug.Log(2);
 	}
 	
 	// Update is called once per frame
