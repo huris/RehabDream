f3c2f855baa7e7b42b50f31ec36c60ad643b2aee
ChenBaye <981090121@qq.com>
2020-10-14 22:25:41 +0800

    Update OSCMonitor.cs

--- Assets/Scripts/Muse/OSC/OSCMonitor.cs
+++ Assets/Scripts/Muse/OSC/OSCMonitor.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using SharpOSC;
 using System.Net;
 using System.Net.Sockets;
+using UnityEngine;
+using System.Threading;
 
 namespace Muse { 
 
@@ -10,7 +12,8 @@ namespace Muse {
     {
         private UDPListener listener;   //UDP监听器
         public Boolean stream = false; //当前是否正在传输
-        public readonly int port = 5000;    //监听本地端口
+        public readonly int port;    //监听本地端口
+        MuseMessage StandardMessage;
 
          // 得到标准Muse数据后调用
         private Action<MuseMessage> _Callback;
@@ -32,15 +35,16 @@ namespace Muse {
         // 监听端口
         private void OSCMonitor_Load(object sender, EventArgs e)
         {
-            HandleOscPacket callback = (OscPacket packet ) => OscPacketHandle(packet);
+            HandleOscPacket callback = (OscPacket packet) => { OscPacketHandle(packet); };
 
             //传入 端口号port、回调函数callback，当UDP监听到数据时，将调用callback处理packet
             listener = new UDPListener(port, callback);
         }
 
         // UDPListener调用OscPacketHandle处理OscPacket
-        private void OscPacketHandle(OscPacket packet)
+        private async void OscPacketHandle(OscPacket packet)
         {
+            Debug.Log("Transmitting...");
             //OSCBundle为OSCPacket的子类
             OscBundle bundle = (OscBundle)packet;
 
@@ -51,13 +55,26 @@ namespace Muse {
             foreach (OscMessage message in bundle.Messages)
             {
                 // 得到标准格式的Muse数据
-                MuseMessage StandardMessage = new MuseMessage(dt, message.Address, message.Arguments);
+                StandardMessage = new MuseMessage(dt, message.Address, message.Arguments);
                 //Console.WriteLine(StandardMessage.ToString());
 
                 // 此处调用委托传出Muse数据
-                _Callback?.Invoke(StandardMessage);
+
+                if (StandardMessage.IsWaveBand())
+                {
+                    Thread t = new Thread(callback);
+                    Debug.Log("Callback1...");
+                    t.Start();
+                }
+                
             }
 
+        }
+
+        private void callback()
+        {
+            Debug.Log("Callback2...");
+            _Callback?.Invoke(StandardMessage);
         }
 
 
